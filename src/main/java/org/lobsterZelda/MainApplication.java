5a1e8581c8edb5d5e7bf2d32589550e3f338a582
Lobsterzelda <tersaderwaslink@gmail.com>
2024-09-22 16:55:46 -0400

    Set up project skeleton

---
+++ src/main/java/org/lobsterZelda/MainApplication.java
@@ -0,0 +1,12 @@
+package org.lobsterZelda;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+//Entrypoint for Java server.
+@SpringBootApplication
+public class MainApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(MainApplication.class, args);
+    }
+}
