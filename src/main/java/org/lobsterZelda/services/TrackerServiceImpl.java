48be88caad528058096995ba935bc8966fb2721c
Lobsterzelda <tersaderwaslink@gmail.com>
2024-09-23 20:21:43 -0400

    Replaced random generation of bytes with constant array of chars used to select a random character

--- src/main/java/org/lobsterZelda/services/TrackerServiceImpl.java
+++ src/main/java/org/lobsterZelda/services/TrackerServiceImpl.java
@@ -5,7 +5,6 @@ import jakarta.servlet.http.HttpServletResponse;
 import org.lobsterZelda.repositories.TrackerRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 
-import java.security.NoSuchAlgorithmException;
 import java.security.SecureRandom;
 import java.util.Map;
 
@@ -43,6 +42,18 @@ public class TrackerServiceImpl implements TrackerService {
         jwtTrackerEditToken.setMaxAge(Integer.MAX_VALUE - 2);
         httpServletResponse.addCookie(jwtTrackerEditToken);
     }
+
+    private static final char[] alphaNumericCharsArray = {
+            '0', '1', '2', '3', '4', '5', '6', '7', '8', '9',
+            'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j',
+            'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't',
+            'u', 'v', 'w', 'x', 'y', 'z',
+            'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J',
+            'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T',
+            'U', 'V', 'W', 'X', 'Y', 'Z'
+    };
+    private static final int ALPHA_NUMERIC_CHAR_ARRAY_SIZE = 62;
+
     // Public token should be 16 random alphanumeric characters.
     private String generateNewPublicTrackerID() {
         try {
@@ -50,15 +61,8 @@ public class TrackerServiceImpl implements TrackerService {
             int charsGenerated = 0;
             StringBuilder newPublicTrackerId = new StringBuilder();
             while (charsGenerated < PUBLIC_TRACKER_ID_SIZE) {
-                char nextRandChar = (char) ((secureRandom.nextInt() % 75) + 48);
-                if (
-                        (nextRandChar >= '0' && nextRandChar <= '9') ||
-                                (nextRandChar >= 'a' && nextRandChar <= 'z') ||
-                                (nextRandChar >= 'A' && nextRandChar <= 'Z')
-                ) {
-                    newPublicTrackerId.append(nextRandChar);
-                    ++charsGenerated;
-                }
+                char nextRandChar = alphaNumericCharsArray[secureRandom.nextInt() % ALPHA_NUMERIC_CHAR_ARRAY_SIZE];
+                ++charsGenerated;
             }
             return newPublicTrackerId.toString();
         } catch (Exception e)
